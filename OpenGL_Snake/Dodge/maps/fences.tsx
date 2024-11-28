001b0b658e0fce197e2faf6e36b5a70ae41ce062
dnosur <45685967+dnosur@users.noreply.github.com>
2024-11-28 12:35:13 +0200

    add game content

---
+++ OpenGL_Snake/Dodge/maps/fences.tsx
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="fences" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../images/mystic_woods/sprites/tilesets/fences.png" trans="ffffff" width="320" height="320"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FenceColission" type="FenceColissions" x="9" y="1" width="13" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FenceColission" type="FenceColissions" x="1" y="8" width="31" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FenceColission" type="FenceColissions" x="0" y="8" width="31" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FenceColission" type="FenceColissions" x="9" y="0" width="13" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FenceColission" type="FenceColissions" x="12.75" y="0.75">
+    <polygon points="0,0 7,0.125 19.125,5.125 19,11.25 8.75,11.125 8.875,30.75 -1.375,30.75 -1.625,1.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="3">
+   <object id="3" name="FenceColission" type="FenceColissions" x="0" y="1" width="32" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="3">
+   <object id="2" name="FenceColission" type="FenceColissions" x="-0.25" y="5.625">
+    <polygon points="0,0 11.125,-4.5 18.875,-4.5 20.125,-2.375 20.25,1.125 20.125,16.875 18.25,26.5 11.5,26.375 9.75,22.375 9.25,13.25 11.125,10.625 10.25,6.25 0.125,6"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="3">
+   <object id="3" name="FenceColission" type="FenceColissions" x="10" y="0" width="13" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FenceColission" type="FenceColissions" x="8" y="0" width="13" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="4">
+   <object id="6" name="FenceColission" type="FenceColissions" x="12.2885" y="0.534283">
+    <polygon points="0,0 7.21282,-0.0890472 9.34996,2.49332 9.52805,12.8228 7.65806,13.4461 7.47996,17.2752 9.34996,19.3232 11.309,22.3508 19.5904,22.4399 19.5013,28.317 7.65806,30.0089 0.267142,30.098 -1.60285,21.6385 -1.60285,18.9671 1.5138,16.6518 1.5138,12.7337 -0.0890472,12.5557 -1.06857,6.05521 -1.24666,2.31523"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FenceColission" type="FenceColission" x="0" y="18" width="32" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="3">
+   <object id="2" name="FenceColission" type="FenceColissions" x="19.125" y="29.625">
+    <polygon points="0,0 1,-6.625 -1,-11.75 0.5,-23 0.75,-27 -2.375,-29.875 -6.75,-29.875 -10.125,-27.25 -10.25,-24 -8.75,-16.75 -8.125,-11.375 -10.375,-9.375 -11.5,-6.625 -18.875,-6.625 -18.75,-1.125 -9.5,-1 -7.875,1.375 -1.875,1.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
